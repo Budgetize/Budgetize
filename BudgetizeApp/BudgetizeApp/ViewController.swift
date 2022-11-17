26ad6b36d9314ee0cc43eb030a2ce3adb121c0ae
jwils133 <jwils133@fiu.edu>
2022-11-17 12:31:27 -0500

    Created xcode project
    Created a new xcode project with a dummy launch screen

---
+++ BudgetizeApp/BudgetizeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetizeApp
+//
+//  Created by Jada Wilson on 11/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
