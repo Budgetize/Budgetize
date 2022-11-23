a5b28435f0bc954f4293fe38f65068735e5d40d9
ZackarySantana <zackzackyack@gmail.com>
2022-11-22 22:44:03 -0500

    Added login and sign up logic

--- BudgetizeApp/BudgetizeApp/LoginViewController.swift
+++ BudgetizeApp/BudgetizeApp/LoginViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onLogin(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameTextField.text!, password: passwordTextField.text!) {(user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription ?? "OH NO")")
+            }
+        }
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        user.signUpInBackground {(success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error \(error?.localizedDescription ?? "OH NO")")
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
