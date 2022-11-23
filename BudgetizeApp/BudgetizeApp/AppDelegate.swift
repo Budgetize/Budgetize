c875efe12cdf87a63a1fd1e9556a738dd2811a75
ZackarySantana <zackzackyack@gmail.com>
2022-11-22 21:39:36 -0500

    Added parse keys

--- BudgetizeApp/BudgetizeApp/AppDelegate.swift
+++ BudgetizeApp/BudgetizeApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "yr6cVBT02p1arKb3AXFpWdM71y3Ee2DeAMhBxZx2" // <- UPDATE
+                $0.clientKey = "ZJr471ktjVq4054ShIrXLtMPQJFHSxIRSMJUoq34" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
